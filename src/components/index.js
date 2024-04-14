581f09cc70295fe60eaeae845721fa8632845ed9
anupam-roy-swe <roya55324@gmail.com>
2024-04-15 01:07:00 +0600

    all commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,27 @@
-import {EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas} from "./canvas";
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-import Home from "./Home";
+import About from "./About";
 import Tech from "./Tech";
 import Experience from "./Experience";
-import Project from "./Project";
+import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
 
 export {
-	Hero,
-	Navbar,
-	Home,
-	Tech,
-	Experience,
-	Project,
-	Feedbacks,
-	Contact,
-	EarthCanvas,
-	BallCanvas,
-	ComputersCanvas,
-	StarsCanvas,
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
 };
